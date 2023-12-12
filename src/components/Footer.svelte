0c585f1df9c655cc47a7a7448b2263ddabc49ddd
Maikon Alexandre Dos Santos Sousa <maikon@pop-os.localdomain>
2023-12-12 18:19:12 -0300

    🏹 change footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,5 +1,7 @@
 <footer class="block py-4 font-light text-sm text-neutral-900 mx-auto">
 	<span class="cursor-pointer">
-		❤️ Made by <a target="_blank" href="https://www.maikonalexandre.com.br">Maike Dev</a>
+		❤️ Made by <a target="_blank" href="https://www.maikonalexandre.com.br"
+			>Maikon Alexandre © 2023</a
+		>
 	</span>
 </footer>
